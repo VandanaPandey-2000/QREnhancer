227b91c94033ba82ceee273f813614bc8de95a83
VandanaPandey-2000 <vandana15112000@gmail.com>
2025-06-05 15:24:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,6 @@ def download():
     return send_file('static/output.png', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use PORT from env or fallback to 5000
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
