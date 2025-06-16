1994a054d1fe250f9aaf535129ca78d398023a36
VandanaPandey-2000 <vandana15112000@gmail.com>
2025-06-16 12:02:05 +0530

    Update app.py, for vercel

--- app.py
+++ app.py
@@ -102,6 +102,7 @@ def download():
     
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # use PORT from env or fallback to 5000
+    port = int(os.environ.get("PORT", 5000))  # Vercel uses dynamic ports
+    # port = int(os.environ.get("PORT", 5000))  # use PORT from env or fallback to 5000
     app.run(host="0.0.0.0", port=port)
     # app.run(debug=True)
